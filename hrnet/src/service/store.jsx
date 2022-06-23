5768e26318c3c143e051a158b08387a890b4473c
VanesMP <vanessa.MP@outlook.fr>
2022-06-23 18:15:05 +0200

    Updates

--- hrnet/src/service/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import globalReducer from "../service/EmployeeSlice.jsx";
-
-export const store = configureStore({
-  reducer: {
-    globalState: globalReducer,
-  },
-});
-
